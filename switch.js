b2a250c8193c003b42b74737395764ab380967c9
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-05-22 16:11:02 +0530

    Trying Fixes..

--- switch.js
+++ switch.js
@@ -5,26 +5,30 @@ link.rel = "stylesheet";
 document.getElementsByTagName("head")[0].appendChild(link);
 
 function injectButtons() {
-  nav = document.getElementsByClassName('pl-2')[0];
-  let db = document.createElement('li');
-      db.innerHTML = `<label class="switch">
-      <input type="checkbox" id="toggle-btn">
-      <span class="slider round"></span>
-      </label>`;
-  if(nav.querySelectorAll('label').length < 1 && (document.URL === "https://github.com/" || document.URL === "https://www.github.com/")) {
-    nav.appendChild(db);
-  }
-  document.getElementById("toggle-btn").addEventListener("click", changeSide);  
+  if (document.URL === "https://github.com/" || document.URL === "https://www.github.com/") {
+    nav = document.getElementsByClassName('pl-2')[0];
+    let db = document.createElement('li');
+        db.innerHTML = `<label class="switch">
+        <input type="checkbox" id="toggle-btn">
+        <span class="slider round"></span>
+        </label>`;
+    if (nav.querySelectorAll('label').length < 1) {
+      nav.appendChild(db);
+    }
+    document.getElementsByClassName('dashboard-sidebar')[0].style.float = "";
+    document.getElementById("toggle-btn").addEventListener("click", changeSide);
+  }  
 }
 
 function changeSide() {
   let side = document.getElementsByClassName('dashboard-sidebar')[0].style.float;
-  if(side === "left") {
-    document.getElementsByClassName('dashboard-sidebar')[0].style.float = 'right';
+  if (side === "left" || side === "") {
+    document.getElementsByClassName('dashboard-sidebar')[0].style.float = "right";
   }
-  else {
-    document.getElementsByClassName('dashboard-sidebar')[0].style.float = 'left';
+  else if (side === "right") {
+    document.getElementsByClassName('dashboard-sidebar')[0].style.float = "left";
   }
+  console.log(side);  
 }
 
 injectButtons();
