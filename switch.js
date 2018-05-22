7702c3d46acef7271b26f7589dab0e1c202ef065
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-05-22 17:14:17 +0530

    try 2

--- switch.js
+++ switch.js
@@ -1,9 +1,11 @@
+// Adding CSS file to the Extension
 var link = document.createElement("link");
 link.href = chrome.extension.getURL("toggle.css");
 link.type = "text/css";
 link.rel = "stylesheet";
 document.getElementsByTagName("head")[0].appendChild(link);
 
+// Injecting Buttons
 function injectButtons() {
   if (document.URL === "https://github.com/" || document.URL === "https://www.github.com/") {
     nav = document.getElementsByClassName('pl-2')[0];
@@ -12,14 +14,17 @@ function injectButtons() {
         <input type="checkbox" id="toggle-btn">
         <span class="slider round"></span>
         </label>`;
+    
+    // Appending switch button if none exist already.
     if (nav.querySelectorAll('label').length < 1) {
       nav.appendChild(db);
     }
-    document.getElementsByClassName('dashboard-sidebar')[0].style.float = "";
+    
     document.getElementById("toggle-btn").addEventListener("click", changeSide);
   }  
 }
 
+// Flipping Dashboard Sides
 function changeSide() {
   let side = document.getElementsByClassName('dashboard-sidebar')[0].style.float;
   if (side === "left" || side === "") {
