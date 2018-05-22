f4c72dcfcd184874dbfdbde3b45bc2bbffdb810f
Aashutosh Rathi <aashutoshrathi@gmail.com>
2018-05-22 17:56:45 +0530

    Fixes (#4)
    First time fix

--- switch.js
+++ switch.js
@@ -1,30 +1,41 @@
+// Adding CSS file to the Extension
 var link = document.createElement("link");
 link.href = chrome.extension.getURL("toggle.css");
 link.type = "text/css";
 link.rel = "stylesheet";
 document.getElementsByTagName("head")[0].appendChild(link);
 
+// Injecting Buttons
 function injectButtons() {
-  nav = document.getElementsByClassName('pl-2')[0];
-  let db = document.createElement('li');
-      db.innerHTML = `<label class="switch">
-      <input type="checkbox" id="toggle-btn">
-      <span class="slider round"></span>
-      </label>`;
-  if(nav.querySelectorAll('label').length < 1 && (document.URL === "https://github.com/" || document.URL === "https://www.github.com/")) {
-    nav.appendChild(db);
-  }
-  document.getElementById("toggle-btn").addEventListener("click", changeSide);  
+  if (document.URL === "https://github.com/" || document.URL === "https://www.github.com/") {
+    nav = document.getElementsByClassName('pl-2')[0];
+    let db = document.createElement('li');
+        db.innerHTML = `<label class="switch">
+        <input type="checkbox" id="toggle-btn">
+        <span class="slider round"></span>
+        </label>`;
+    
+    // Appending switch button if none exist already.
+    if (nav.querySelectorAll('label').length < 1) {
+      nav.appendChild(db);
+    }
+    
+    document.getElementById("toggle-btn").addEventListener("change", changeSide);
+    //changeSide();
+  }  
 }
 
+// Flipping Dashboard Sides
 function changeSide() {
+  const toggleBtn = document.getElementById('toggle-btn');
   let side = document.getElementsByClassName('dashboard-sidebar')[0].style.float;
-  if(side === "left") {
-    document.getElementsByClassName('dashboard-sidebar')[0].style.float = 'right';
+  if (toggleBtn.checked) {
+    document.getElementsByClassName('dashboard-sidebar')[0].style.float = "right";
   }
   else {
-    document.getElementsByClassName('dashboard-sidebar')[0].style.float = 'left';
+    document.getElementsByClassName('dashboard-sidebar')[0].style.float = "left";
   }
+  console.log(side);  
 }
 
 injectButtons();
